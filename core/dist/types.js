c704e1998e08cbe508ea618519396eaa3afc0d9b
Evs91 <8013869+Evs91@users.noreply.github.com>
2024-11-25 08:33:53 -0500

    Update types.js

--- core/dist/types.js
+++ core/dist/types.js
@@ -5,5 +5,5 @@ var ModelOptions;
 (function (ModelOptions) {
     ModelOptions["gpt_4o"] = "gpt-4o";
     ModelOptions["gpt_4o_mini"] = "gpt-4o-mini";
-    ModelOptions["bakllava"] = "bakllava:latest";
+    ModelOptions["llamavision"] = "llama3.2-vision:11b-instruct-q4_K_M";
 })(ModelOptions || (exports.ModelOptions = ModelOptions = {}));

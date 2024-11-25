e92680f6f8de6551d07a39740e8680695d158219
Evs91 <8013869+Evs91@users.noreply.github.com>
2024-11-25 08:11:48 -0500

    Update index.d.ts

--- core/dist/index.d.ts
+++ core/dist/index.d.ts
@@ -1,2 +1,2 @@
 import { DocumindArgs, DocumindOutput } from "./types";
-export declare const documind: ({ cleanup, concurrency, filePath, llmParams, maintainFormat, model, openaiAPIKey, outputDir, pagesToConvertAsImages, tempDir, }: DocumindArgs) => Promise<DocumindOutput>;
+export declare const documind: ({ cleanup, concurrency, filePath, llmParams, maintainFormat, model, openaiApiEndpoint, openaiAPIKey, outputDir, pagesToConvertAsImages, tempDir, }: DocumindArgs) => Promise<DocumindOutput>;
